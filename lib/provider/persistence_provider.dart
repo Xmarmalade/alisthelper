0b641e1c6eb41829433387c15f32872249cc087b
iiijam <16839488+Xmarmalade@users.noreply.github.com>
2023-06-19 16:23:52 +0800

    feat(version): dependency upgrade
    http 1.0.0 (was 0.13.6) collection 1.17.2 (was 1.17.1)

--- lib/provider/persistence_provider.dart
+++ lib/provider/persistence_provider.dart
@@ -10,7 +10,7 @@ final persistenceProvider = Provider<PersistenceService>((ref) {
 });
 
 // Version of the storage
-const currentAlistHelperVersion = 'v0.1.0';
+const currentAlistHelperVersion = 'v0.1.1';
 const _version = 'ah_current_version';
 
 // App Window Offset and Size info
